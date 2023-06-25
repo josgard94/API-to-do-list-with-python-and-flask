92ac030ca1f540a570dcef81a4867465e1d95686
Edgard Diaz <ediaz@bluepeople.com>
2023-06-25 00:48:45 -0600

    JOS-06 - conecctions file updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from routes.task_routes import task_routes
 import logging
 from database.conecctions import SingletonConnection
-from models.task_model import Task, Base
+from models.task_model import Base
 
 app = Flask(__name__)
 app.register_blueprint(task_routes, url_prefix='/tasks')
@@ -12,5 +12,6 @@ db = SingletonConnection()
 engine = db.get_engine()
 
 Base.metadata.create_all(bind=engine)
+
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
--- database/conecctions.py
+++ database/conecctions.py
@@ -1,21 +1,33 @@
 from sqlalchemy import create_engine
 from config.credentials import credentials
 import logging
+from sqlalchemy.orm import sessionmaker
+
+
 class SingletonConnection:
     _instance = None
+
     def __init__(self) -> None:
         self.logger = logging.getLogger(__name__)
 
     def __new__(cls):
         if not cls._instance:
-            engine = create_engine("mysql+pymysql://"+str(credentials['user'])+":"+str(credentials['password'])+"@"+str(credentials['host'])+":3306/"+str(credentials["database"]))
             cls._instance = super().__new__(cls)
-            cls._instance.engine = engine
+            cls._instance._initialize_engine()
         return cls._instance
 
+    def _initialize_engine(self):
+        self.logger = logging.getLogger(__name__)
+        self.engine = create_engine(
+            f"mysql+pymysql://{credentials['user']}:{credentials['password']}@{credentials['host']}:3306/{credentials['database']}")
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
 
     def get_engine(self):
         try:
-           return self.engine
+            return self.engine
         except Exception as error:
-           self.logger.error("connection error -", str(error))
+            self.logger.error("connection error -", str(error))
+
+    def get_session(self):
+        return self.session
