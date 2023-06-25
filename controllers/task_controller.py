c9609c6705952119b4f8b9d60a9d0c9af7a4060a
Edgard Diaz <ediaz@bluepeople.com>
2023-06-25 00:52:03 -0600

    JOS-08 - Logic to insert task into DB added

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -1,9 +1,27 @@
+from database.conecctions import SingletonConnection
+from sqlalchemy import MetaData
+import logging
+import json
+from models.task_model import Task
+
+
 class TaskManagement:
     def __init__(self):
-        pass
-    
-    def create_tasks(self): 
-        return f"hello from create task controller"
+        self.connection = SingletonConnection()
+        self.session = self.connection.get_session()
+        self.metadata = MetaData()
+        self.logger = logging.getLogger(__name__)
+
+    def create_tasks(self, params):
+        try:
+            task = Task(name=params.get("name"), description=params.get(
+                "description"), priority=params.get("priority"), status=params.get("priority"))
+            self.session.add(task)
+            self.session.commit()
+        except Exception as error:
+            self.logger.info("Error - %s", str(error))
+
+        return f"created task success!!"
 
     def update_task(self):
         pass
--- routes/task_routes.py
+++ routes/task_routes.py
@@ -3,14 +3,16 @@ from controllers.task_controller import TaskManagement
 
 
 task_routes = Blueprint('routes', __name__)
+
+
 class TaskRoutes:
 
     def __init__(self) -> None:
         self.controller = TaskManagement()
 
     def create(self):
-        
-        return self.controller.create_tasks()
+        params = request.get_json()
+        return self.controller.create_tasks(params)
 
     def list_task(self):
         return self.controller.get_tasks()
@@ -21,7 +23,12 @@ class TaskRoutes:
     def update_status_status(self):
         return self.controller.update_status_task()
 
-task_routes.add_url_rule('/create', view_func=TaskRoutes().create, methods=['GET'])
-task_routes.add_url_rule('/list', view_func=TaskRoutes().list_task, methods=['GET'])
-task_routes.add_url_rule('/update', view_func=TaskRoutes().update_task, methods=['PUT'])
-task_routes.add_url_rule('/update/status', view_func=TaskRoutes().update_status_status, methods=['PUT'])
+
+task_routes.add_url_rule(
+    '/create', view_func=TaskRoutes().create, methods=['GET'])
+task_routes.add_url_rule(
+    '/list', view_func=TaskRoutes().list_task, methods=['GET'])
+task_routes.add_url_rule(
+    '/update', view_func=TaskRoutes().update_task, methods=['PUT'])
+task_routes.add_url_rule(
+    '/update/status', view_func=TaskRoutes().update_status_status, methods=['PUT'])
